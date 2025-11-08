e7585a0b3bb4897b6f478636fe2b1352e01fea6a
Simon Vinding Brodersen <simon@3450.dk>
2025-11-08 17:45:00 +0100

    started work on editor

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io::{self, Read};
+
+fn main() {
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:03} \r", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:03}\r\nCharacter: {1:?}\r", b, c);
+        }
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+    }
+}
